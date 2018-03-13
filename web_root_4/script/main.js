6cf8959f7e374dc36eed094a35232bd269b72b73
CesaTor <>
2018-03-13 17:55:43 +0100

    LocalStorage

---
+++ web_root_4/script/main.js
@@ -0,0 +1,55 @@
+// Global Variables ----------------------------------------
+
+var myImage = document.querySelector('img');
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+// END Global Variables ----------------------------------------
+
+
+
+// Functions -----------------------------------------------
+
+// Set username to header
+function setHeader(name) {
+	myHeading.innerHTML = 'Things are cool, ' + name;
+}
+
+// Richiesta Username
+function setUserName() {
+	var myName = prompt('Please enter your name.');
+	localStorage.setItem('name', myName);
+	setHeader(myName);
+}
+
+// END Funtions ---------------------------------------------
+
+
+
+// Listeners ------------------------------------------------
+
+// OnImageClick
+myImage.onclick = () => {
+	let mySrc = myImage.getAttribute('src');
+	if (mySrc === 'assets/img/img.jpg') {
+		myImage.setAttribute('src', 'assets/img/img2.jpg')
+	}
+	else {
+		myImage.setAttribute('src', 'assets/img/img.jpg')
+	}
+}
+
+// OnButtonClick
+myButton.onclick = setUserName;
+
+if (!localStorage.getItem('name')) {
+	setUserName();
+} else {
+	var storedName = localStorage.getItem('name');
+	setHeader(storedName);
+}
+// END Listeners ------------------------------------------------
+
+
+var car = ['Fiat', 'Volvo', 'BMW', 'Mercedes'];
+console.log(car, car.indexOf('BMW'));
